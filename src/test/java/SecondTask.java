e9888081ea538a65d9be6d24bc720f9bb14d09fd
igor romanov <igor.romanovqa@gmail.com>
2020-05-26 01:02:40 +0400

    Added Second Task

---
+++ src/test/java/SecondTask.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        // Ввод начального и конечного чисел в систему
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int firstNum = in.nextInt();
+        System.out.print("Input last number: ");
+        int lastNum = in.nextInt();
+
+        // Ввести переменную i, пройти циклом диапазон введенных значений
+            for(int i = firstNum ; i <= lastNum ; ++i) {
+                if(i % 15 == 0) {
+                    System.out.println("FizzBuzz");
+                }
+                else if(i % 3 == 0) {
+                    System.out.println("Fizz");
+                }
+                else if(i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+                else {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
